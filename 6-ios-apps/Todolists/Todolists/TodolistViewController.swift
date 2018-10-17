c0e09468463169cda6a945ad76dd079084bb16e5
Ariana <arianarsdev@gmail.com>
2018-10-16 22:57:50 -0500

    TodolistItem model was added

--- 6-ios-apps/Todolists/Todolists/TodolistViewController.swift
+++ 6-ios-apps/Todolists/Todolists/TodolistViewController.swift
@@ -9,35 +9,37 @@
 import UIKit
 
 class TodolistViewController: UITableViewController {
-
+    var items = [TodolistItem]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let item1 = TodolistItem()
+        item1.text = "Run 5K"
+        items.append(item1)
+        
+        let item2 = TodolistItem()
+        item2.text = "Take a cold shower"
+        items.append(item2)
+        
+        let item3 = TodolistItem()
+        item3.text = "Learn iOS Development"
+        items.append(item3)
     }
     
     // MARK: - Table View Data Source
     // This method is like the getItemCount() in Android RecyclerView adapter
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 5
+        return items.count
     }
     
     // This method is like the onBindViewHolder() in Android RecyclerView adapter
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "TodolistItem", for: indexPath)
+        let item = items[indexPath.row]
         
-        let label = cell.viewWithTag(1000) as! UILabel
-        
-        if indexPath.row == 0 {
-            label.text = "Run 5K"
-        } else if indexPath.row == 1 {
-            label.text = "Take a cold shower"
-        } else if indexPath.row == 2 {
-            label.text = "Work on the Android app"
-        } else if indexPath.row == 3 {
-            label.text = "Learn iOS Development"
-        } else if indexPath.row == 4 {
-            label.text = "Meditate"
-        }
+        configureText(for: cell, with: item)
+        configureCheckmark(for: cell, with: item)
         
         return cell
     }
@@ -45,13 +47,24 @@ class TodolistViewController: UITableViewController {
     // MARK: - Table View Delegate
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         if let cell = tableView.cellForRow(at: indexPath) {
-            if cell.accessoryType == .none {
-                cell.accessoryType = .checkmark
-            } else {
-                cell.accessoryType = .none
-            }
+            let item = items[indexPath.row]
+            item.toggleChecked()
+            configureCheckmark(for: cell, with: item)
         }
         tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+    func configureText(for cell: UITableViewCell, with item: TodolistItem) {
+        let label = cell.viewWithTag(1000) as! UILabel
+        label.text = item.text
+    }
+    
+    func configureCheckmark(for cell: UITableViewCell, with item: TodolistItem) {
+        if item.checked {
+            cell.accessoryType = .checkmark
+        } else {
+            cell.accessoryType = .none
+        }
     }
 
 }
